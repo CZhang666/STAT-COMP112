b4cd279e3f6ee90082bbbc230b38f57a89cda63a
CZhang666 <2265981775@qq.com>
2019-11-12 14:15:08 -0600

    add shinny app

---
+++ 9_Shiny_app/app.R
@@ -0,0 +1,44 @@
+
+library(shiny)
+library(tidyverse)
+library(readr)
+library(rsconnect)
+
+rsconnect::setAccountInfo(name='charles-zhang',
+                          token='890F803ACBC5DEE28D543C5A1082EE7B',
+                          secret='K+48LSVwcRAMrEn3NIbv69Gwtd3QhCrsynwFscoo')
+
+board_games <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-03-12/board_games.csv")
+
+ui <- fluidPage(
+  sliderInput(inputId = "year", label = "Year Published",
+              min = 1950, max = 2019, value = c(1950,2019),sep = ""),
+  textInput("max_players", "Max Players", value = "", placeholder = "1"),
+  submitButton(text = "Create my plot!"),
+  plotOutput(outputId = "timeplot")
+)
+
+server <- function(input, output) {
+  output$timeplot <- renderPlot({
+    board_games %>% 
+      filter(max_players == input$max_players) %>% 
+      ggplot(aes(x = year_published, y = average_rating))+
+      geom_point()+
+      scale_x_continuous(limits = input$year) +
+      theme_classic()
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
